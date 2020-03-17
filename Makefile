627fa77e6aa9af130fc9bc574f902a293ef89f31
shenmuyu <502553633@qq.com>
2020-03-17 18:18:29 +0800

    更新项目结构，通过第一个测试case

--- Makefile
+++ Makefile
@@ -1,9 +1,6 @@
 .PHONY: app test
 
 FLASK_APP_NAME='flask-dev/manage.py'
-Ali_OSS_AK_ID='<你的access_key_id>'
-Ali_OSS_AK_SECRET='<你的access_key_secret>'
-Ali_OSS_ENDPOINT='<你的服务器访问域名>'
 
 all: app
 
@@ -11,5 +8,4 @@ app:
 	FLASK_APP=$(FLASK_APP_NAME) FLASK_ENV='default' flask run --host=0.0.0.0
 
 test:
-	FLASK_APP=$(FLASK_APP_NAME) FLASK_ENV='testing' OSS_AK_ID=$(Ali_OSS_AK_ID) \
-	OSS_AK_SECRET=$(Ali_OSS_AK_SECRET) OSS_ENDPOINT=$(Ali_OSS_ENDPOINT) pytest
+	FLASK_APP=$(FLASK_APP_NAME) FLASK_ENV='testing' pytest
--- flask-dev/tests/oss_test/test_api_access.py
+++ flask-dev/tests/oss_test/test_api_access.py
@@ -1,7 +1,12 @@
 import pytest
 from flask import current_app
-from app.oss import Bucket
 
 
-def test_get_bucket_lists():
-    bucket = Bucket('lxq-photo')
+def test_access_bucket_success(app_content):
+    import oss2
+    access_key_id = current_app.config['OSS_AK_ID']
+    access_key_secret = current_app.config['OSS_AK_SECRET']
+    endpoint = current_app.config['OSS_ENDPOINT']
+    bucket = oss2.Bucket(oss2.Auth(access_key_id, access_key_secret), endpoint, 'lxq-photo')
+    bucketinfo = bucket.get_bucket_info()
+    assert bucketinfo.name == 'lxq-photo'
